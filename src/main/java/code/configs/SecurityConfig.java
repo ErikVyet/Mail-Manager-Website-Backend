adf3d94f44945d25945df3f2a50fed12a3d93eea
Vyet <141729321+ErikVyet@users.noreply.github.com>
2026-08-21 12:03:19 +0700

    Remove authorization for health check

--- src/main/java/code/configs/SecurityConfig.java
+++ src/main/java/code/configs/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/vletter/api/v1/**").authenticated()
+                .requestMatchers("/vletter/api/v1/health/**").permitAll()
                 .anyRequest().permitAll()
             )
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
